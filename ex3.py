2b364649e42d714d80ba4f11e9eea459a71129ff
GageOfLeon <gdeleon@tonicdesign.com>
2017-06-14 11:14:52 -0400

    Notes for ex3.py

--- ex3.py
+++ ex3.py
@@ -2,12 +2,14 @@ print "I will now count my chickens:"
 
 print "Hens", 25 + 30 / 6
 print "Roosters", 100 - 25 * 3 % 4
+# When '/, -, +, *, %' are introduced, it automaticly does the math
 
 print "Now I will count the eggs:"
 
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
+# Not only does it automaticly do the math, it also tells wheither a problem such as 2>1 is either true or false
 
 print 3 + 2 < 5 - 7
 
