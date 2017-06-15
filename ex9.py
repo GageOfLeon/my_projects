0bdc9d74d313d91876a172f46be7178d409a21c2
GageOfLeon <gdeleon@tonicdesign.com>
2017-06-15 09:42:27 -0400

    Added notes to ex9.py

--- ex9.py
+++ ex9.py
@@ -1,7 +1,9 @@
 # Here's some new strange stuff, remember type it exactly.
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
+# This puts the days of the week together on a variable. If used, it'll show the days
 months = "Jan\nfeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#This variable shows the months of the year.\n makes the whatever is to the right of it go on a new line.
 
 print "Here are the days: ", days
 print "Here are the months: ", months
