5769678f388a7d593a5cc830a35de48a9417e9b3
GageOfLeon <gdeleon@tonicdesign.com>
2017-06-15 10:46:33 -0400

    Added notes to ex15.py

--- ex15.py
+++ ex15.py
@@ -3,9 +3,11 @@ from sys import argv
 script, filename = argv
 
 txt = open(filename)
+# this variable will open a file that will be placed since argv is active
 
 print "Here's your file %r:" % filename
 print txt.read ()
+#reads the txt file and if inputed again it will read it again
 
 print "Here's your file %r:" % filename
 file_again = raw_input ("> ")
