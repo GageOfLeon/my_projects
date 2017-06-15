0b241301a249c0d1574296e666405ca240720536
GageOfLeon <gdeleon@tonicdesign.com>
2017-06-15 10:28:47 -0400

    Added notes for ex13.py

--- ex13.py
+++ ex13.py
@@ -1,8 +1,10 @@
 from sys import argv
-
+# The first line allows use to add features from the python feature set. argv is the "arguement variable" It holds arguements that is pass to the script
 script, first, second, third = argv
+# The inputs here are saved until called on since its an argv
 
 print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
 print "Your third variable is:", third
+# Inputs that were stored are now printed out due to the print command
