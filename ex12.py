775276c227cbb2d1e57634db79895280c8a95ff4
GageOfLeon <gdeleon@tonicdesign.com>
2017-06-15 10:14:12 -0400

    Added notes to ex12.py

--- ex12.py
+++ ex12.py
@@ -3,3 +3,4 @@ height = raw_input("How tall are you?")
 weight = raw_input("How much do you weigh")
 
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+# So it %r is does the same thing as %s except their is one difference. If %r it'll have '' but if %s it won't.
