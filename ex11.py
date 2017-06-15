9075503b850bd255710a6e745623c6a97aab69eb
GageOfLeon <gdeleon@tonicdesign.com>
2017-06-15 10:03:20 -0400

    Added notes for ex11.py

--- ex11.py
+++ ex11.py
@@ -4,5 +4,7 @@ print "How tall are you?",
 height = raw_input ()
 print "How much do you weigh?",
 weight = raw_input ()
+# the variables are determined by user input. The command raw_input() allows the user to input what they desire
 
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+# %r is connected with the variables. It seems to print out the raw_input that the user had typed up. % performs a string format
